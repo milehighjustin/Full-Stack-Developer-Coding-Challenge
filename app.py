62c79a3ada9db029a4d03925f201c34a94a6f52e
Justin Yarusso <justin.yarusso@outlook.com>
2021-02-24 16:47:42 -0600

    env

--- app.py
+++ app.py
@@ -1,66 +1,111 @@
 from flask import Flask, request, jsonify
 import json
+import mysql.connector as mysql
+import bcrypt
+import secrets
+import os
+
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+db = mysql.connect(
+            host = os.getenv('DBHOST'),
+            user = os.getenv('DBUSER'),
+            passwd = os.getenv('DBPASS'),
+            database = os.getenv('DBNAME')
+        )
 
-    # For debugging
-    print(f"got name {name}")
+def getuserbyemail(email):
+    try:
+        cursor = db.cursor(buffered=True)
+        query = "SELECT * from users WHERE email = %s"
+        values = (email, )
+        cursor.execute(query, values)
+        return cursor.fetchone()
+    except:
+        return "error"
 
-    response = {}
+def createtoken(user):
+    token = secrets.token_hex(20)
+    query = "INSERT into tokens (userid, token) VALUES (%s, %s)"
+    values = (user, token)
+    cursor = db.cursor(buffered=True)
+    cursor.execute(query, values)
+    db.commit()
+    return token
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
+
+@app.route('/login/', methods=['POST'])
+def login_user():
+    req_data = request.get_json()
+    if req_data:
+        try:
+            record = getuserbyemail(req_data['useremail'])
+            storedpw = record[4]
+            if(bcrypt.checkpw(req_data['userpass'].encode(), storedpw.encode())):
+                return jsonify({
+                    "status": f"success",
+                    "message": createtoken(record[0])
+                })
+            else:
+                return jsonify({
+                    "status": f"error",
+                    "message": f"Invalid Login"
+                })
+        except:
+            return jsonify({
+                "status": "Invalid Login"
+            })
     else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+        return jsonify({
+            "status": f"error",
+            "message": f"Invalid Request"
+        })
 
-    # Return the response in json format
-    return jsonify(response)
 
 @app.route('/adduser/', methods=['POST'])
 def add_user():
     req_data = request.get_json()
     if req_data:
         try:
-            req_data['userfirst']
+            userf = req_data['userfirst']
         except:
             return jsonify({
-                "status": f"error",
-                "message": f"Please include user's first name"
+                "status": "error",
+                "message": "Please include user's first name"
             })
         try:
-            req_data['userlast']
+            userl = req_data['userlast']
         except:
             return jsonify({
-                "status": f"error",
-                "message": f"Please include user's last name"
+                "status": "error",
+                "message": "Please include user's last name"
             })
         try:
-            req_data['useremail']
+            useremail = req_data['useremail']
         except:
             return jsonify({
-                "status": f"error",
-                "message": f"Please include user's email"
+                "status": "error",
+                "message": "Please include user's email"
             })
         try:
-            req_data['userpass']
+            userpass = req_data['userpass']
         except:
             return jsonify({
-                "status": f"error",
-                "message": f"Please include a strong password"
+                "status": "error",
+                "message": "Please include a strong password"
             })
+        # We good
+        hashpw = bcrypt.hashpw(userpass.encode(), bcrypt.gensalt())
+        cursor = db.cursor()
+        query = "INSERT into users (userf, userl, email, password) VALUES (%s, %s, %s, %s)"
+        values = (userf, userl, useremail, hashpw)
+        cursor.execute(query, values)
+        db.commit()
+        getuser = getuserbyemail(useremail)
+        token = createtoken(getuser[0])
         return jsonify({
-            "Message": f"Welcome {req_data['userfirst']} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
+            "status": "success",
+            "message": token
         })
 
     else:
@@ -70,7 +115,7 @@ def add_user():
 
 @app.route('/')
 def index():
-    return "<h1>Welcomet to SpaceFlask 🚀</h1>"
+    return "<h1>Welcome to SpaceFlask 🚀</h1>"
 
 if __name__ == '__main__':
     app.run(threaded=True)
